f8842f6515dd31fa136a152d534839467940ef87
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-03-22 16:40:47 -0700

    testing

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
-
-from flask import Flask
+from PyPDF2 import PdfReader
+from flask import Flask, request
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
     return f"Hello {name}!"
+
+@app.route('/files', methods=['POST'])
+def file_parser():
+    data = request.body
+    return data
 
 
 if __name__ == "__main__":
