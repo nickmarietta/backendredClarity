a1b523d30934f3c4d714f3df33bbed4b87c902d0
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-03-22 20:34:28 -0700

    implementing updates

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, request, jsonify
 from PyPDF2 import PdfReader
 from google import genai
 from google.genai import types
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/", methods={"GET"})
 def hello_world():
